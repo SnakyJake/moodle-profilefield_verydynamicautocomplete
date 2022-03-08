e1be89de9a2c20a9cd1358ad4e65211997757d3e
Jakob Heinemann <jakob@jakobheinemann.de>
2022-03-08 04:36:28 +0100

    added userid placeholder for convenience

--- locallib.php
+++ locallib.php
@@ -26,6 +26,8 @@ function verydynamicautocomplete_profilefield_fix_sql(&$sql,$user){
     //profile_get_user_fields_with_data($userid)
     $pfsql = "SELECT shortname,data from {user_info_field} f left join {user_info_data} d on d.userid=:userid and d.fieldid=f.id";
     $profilefields = $DB->get_records_sql_menu($pfsql,array('userid'=>$user->id));
+
+    //@todo: #1 put this on top and check for matches so we dont need the database query above
     preg_match_all('/\[([a-z][a-z0-9_]*)\]/', $sql, $matches);
 
     $wants_fullname = false;
@@ -36,7 +38,9 @@ function verydynamicautocomplete_profilefield_fix_sql(&$sql,$user){
             $name = substr($field,$profile_field_len, strlen($field)-$profile_field_len);
             $field = $profilefields[$name];
         } else {
-            if($field == "fullname"){
+            if($field == "userid"){ //help the sql manager if he uses userid instead of id
+                $field = $user->id;
+            } elseif($field == "fullname"){
                 $wants_fullname = true;
                 // before moodle 3.11: 
                 // $field = implode(get_all_user_name_fields(),",");
